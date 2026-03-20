94613afd837a02a472b6174a8de50a595de9657f
Sami Telo <samitelo@gmail.com>
2026-03-20 23:32:50 +0000

    fix(auth): wrap verification-otp with Suspense to fix build issue

--- src/app/auth/verification-otp/page.tsx
+++ src/app/auth/verification-otp/page.tsx
@@ -1,28 +1,29 @@
 import { VerificationForm } from "@/features/auth/components/verification-form";
 import Image from "next/image";
+import { Suspense } from "react";
 
 export default function LoginPage() {
   return (
     <div className="grid min-h-screen lg:grid-cols-2">
-    
-    {/*----------------------------------------------------
+      {/*----------------------------------------------------
          BLOC LEFT: Form
       -----------------------------------------------------------*/}
       <div className="order-2 lg:order-1 flex flex-col gap-4 p-6 md:p-10">
         {/* Form centré */}
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-115">
-            <VerificationForm/>
+            <Suspense fallback={<div>Chargement...</div>}>
+              <VerificationForm />
+            </Suspense>
           </div>
         </div>
       </div>
 
-       {/*---------------------------------------------------
+      {/*---------------------------------------------------
           BLOC RIGHT: Image
        -------------------------------------------------------*/}
       <div className="order-1 lg:order-2 relative bg-[url('/assets/register-img.jpg')] bg-cover bg-center min-h-100 lg:min-h-screen flex items-center justify-center">
-
-      {/* Logo */}
+        {/* Logo */}
         <div className="absolute top-6 right-6 flex items-center gap-3 p-3 md:p-4 bg-white/10 backdrop-blur-sm border border-white/10 shadow-xl rounded-2xl">
           <a href="#" className="flex items-center gap-3 font-semibold text-lg">
             <div className="w-9 h-9 relative">
@@ -33,7 +34,9 @@ export default function LoginPage() {
                 className="object-contain rounded-xl"
               />
             </div>
-            <span className="text-white text-sm md:text-[15px] ">Football Club</span>
+            <span className="text-white text-sm md:text-[15px] ">
+              Football Club
+            </span>
           </a>
         </div>
       </div>
