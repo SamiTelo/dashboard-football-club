1a40e0d46ff64136ebec55898eeb03ec8a155737
Sami Telo <samitelo@gmail.com>
2026-04-17 16:19:55 +0000

    feat(players): add dashboard preview with pagination and unified table UI
    - Added PlayersPreview component for dashboard
    - Integrated full PlayersTable UI in preview mode
    - Added search + lightweight pagination in preview
    - Reused existing pagination logic style for consistency
    - Improved dashboard layout with hero, stats, and preview section

--- src/features/dashbaord/components/dashbaord-main.tsx
+++ src/features/dashbaord/components/dashbaord-main.tsx
@@ -1,49 +1,54 @@
 "use client";
 
 import { StatsCards } from "@/features/players/components/stats-cards";
-import { PlayersFilters } from "@/features/players/components/players-filters";
 import { Hand } from "lucide-react";
 import { useAuth } from "@/features/auth/hooks/useAuth";
 import { Skeleton } from "@/components/ui/skeleton";
-import PlayersList from "@/features/players/players-list";
+import { PlayersPreview } from "@/features/players/components/players-preview";
 
-export default function DashboardMian() {
+export default function DashboardMain() {
   const { user, loading } = useAuth(true);
 
-  const userName = user ? `${user.firstName} ${user.lastName}` : "";
+  const userName = user
+    ? `${user.firstName} ${user.lastName}`
+    : "";
 
   return (
     <div className="p-0 bg-[#F8F7FA] min-h-screen text-[13px] md:text-[14px] font-sans text-[#5d596c]">
+
       <p className="mt-6">Tableau de bord /</p>
       <br />
 
-       {/* Hero */}
+      {/* HERO */}
       <div className="bg-green-100 py-4 px-4 sm:px-6 mb-6 rounded-xl">
-        <div className="flex flex-col sm:flex-row sm:items-center gap-2 sm:gap-3 text-sm sm:text-base md:text-[14px] leading-relaxed">
-          <div className="flex items-center">
-            <Hand className="text-green-500 shrink-0" />
-          </div>
-          <p className="flex flex-wrap items-center gap-x-1 gap-y-1">
+        <div className="flex items-center gap-2 sm:gap-3">
+          <Hand className="text-green-500" />
+
+          <p className="flex gap-1">
             <span>Bonjour</span>
 
             {loading ? (
               <Skeleton className="h-4 w-24 rounded-md" />
             ) : (
-              <strong className="text-gray-800">{userName}</strong>
+              <strong>{userName}</strong>
             )}
 
             <span>, bienvenue sur</span>
-            <span className="text-green-600 font-semibold">Football Club</span>
+            <span className="text-green-600 font-semibold">
+              Football Club
+            </span>
           </p>
         </div>
       </div>
 
+      {/* STATS */}
       <StatsCards />
 
-      <div className="bg-white rounded-lg border border-gray-100">
-        <PlayersFilters />
-        <PlayersList/>
+      {/* PLAYERS PREVIEW */}
+      <div className="bg-white rounded-lg border border-gray-100 mt-6">
+        <PlayersPreview/>
       </div>
+
     </div>
   );
 }
